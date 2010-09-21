8816038771915034accab509bcb3f0c2e11cb919
Chuck Vose <vosechu@metaltoad.com>
2010-09-20 20:06:44 -0700

    quick cleanup and commenting

--- twitter-caching.php
+++ twitter-caching.php
@@ -46,10 +46,19 @@ function relativeTime($time)
 	and other cache code from http://wiki.kientran.com/doku.php?id=projects:twitterbadge
 */
 function parse_cache_feed($usernames, $limit) {
-	$username_for_feed = str_replace(" ", "+OR+from%3A", $usernames);
+  // Generate an array based on a comma or space seperated list
+  $users = explode(" ", explode(", ", $usernames));
+  
+  // Assemble the GET request parameters
+	$username_for_feed = implode('+OR+from%3A', $users);
 	$feed = "http://search.twitter.com/search.atom?q=from%3A" . $username_for_feed . "&rpp=" . $limit;
+	
+	// Build the file path for the file cache
 	$usernames_for_file = str_replace(" ", "-", $usernames);
 	$cache_file = dirname(__FILE__).'/cache/' . $usernames_for_file . '-twitter-cache';
+	
+	// TODO: Check for directory existence and directory permissions
+	
 	$last = filemtime($cache_file);
 	$now = time();
 	$interval = 600; // ten minutes
@@ -63,29 +72,29 @@ function parse_cache_feed($usernames, $limit) {
 		} else {
 			// we got good results from twitter
 			echo "<!-- SUCCESS: Twitter feed used to update cache file -->";
+			
+			// Open the file, serialize the code, then write it out to disk.
 			$cache_static = fopen($cache_file, 'wb');
 			fwrite($cache_static, serialize($cache_rss));
 			fclose($cache_static);
 		}
-		// read from the cache file
-		$rss = @unserialize(file_get_contents($cache_file));
 	}
-	else {
-		// cache file is fresh enough, so read from it
-		echo "<!-- SUCCESS: Cache file was recent enough to read from -->";
-		$rss = @unserialize(file_get_contents($cache_file));
-	}
-	// clean up and output the twitter feed
-	$feed = str_replace("&amp;", "&", $rss);
-	$feed = str_replace("&lt;", "<", $feed);
-	$feed = str_replace("&gt;", ">", $feed);
-	$clean = explode("<entry>", $feed);
-	$clean = str_replace("&quot;", "'", $clean);
-	$clean = str_replace("&apos;", "'", $clean);
-	$amount = count($clean) - 1;
-	if ($amount) { // are there any tweets?
-		for ($i = 1; $i <= $amount; $i++) {
-			$entry_close = explode("</entry>", $clean[$i]);
+	// TODO: Find a way to fix errors without @
+	// read from the cache file even if we got new results from the feed
+	$rss = unserialize(file_get_contents($cache_file));
+
+	 // // clean up and output the twitter feed
+	 // $feed = str_replace("&amp;", "&", $rss);
+	 // $feed = str_replace("&lt;", "<", $feed);
+	 // $feed = str_replace("&gt;", ">", $feed);
+	 // $clean = explode("<entry>", $feed);
+	 // $clean = str_replace("&quot;", "'", $clean);
+	 // $clean = str_replace("&apos;", "'", $clean);
+	$tweets = htmlentities($rss);
+	
+	if (count($tweets) > 0) { // are there any tweets?
+	  foreach ($tweets as $tweet) {
+	    $entry_close = explode("</entry>", $clean[$i]);
 			$clean_content_1 = explode("<content type=\"html\">", $entry_close[0]);
 			$clean_content = explode("</content>", $clean_content_1[1]);
 			$clean_name_2 = explode("<name>", $entry_close[0]);
@@ -109,7 +118,7 @@ function parse_cache_feed($usernames, $limit) {
 					</p>
 				</blockquote>
 			<?php
-		}
+	  }
 	} else { // if there aren't any tweets
 		?>
 			<blockquote>
