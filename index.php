85d1607105a3566f6db347d8e6664f6e2a11269c
Scott Vandehey <fake-scott@spaceninja.com>
2010-09-20 22:17:02 -0700

    minor cleanup and commenting

--- index.php
+++ index.php
@@ -8,10 +8,12 @@
       background: #f1f1f1;
     }
     body {
+      background: #fff;
       color: #333;
       font: 87.5%/1.5 Calibri, sans-serif; /* 14px */
       width: 640px;
       margin: 50px auto;
+      padding: 20px;
     }
     a, a:link, a:visited { color: #333; }
     a:hover, a:focus, a:active { color: #666; }
@@ -22,8 +24,10 @@
 <?php
   require_once('twitter-caching.inc');
   
+  // Here's the simple way to get a user's 5 most recent tweets
   parse_cache_feed("spaceninja");
   
+  // // Here's the more verbose method, with all the options explained
   // $usernames = "vosechu spaceninja"; // Space delimited list of handles
   // $limit = "10"; // Number of tweets to pull in
   // $display_username = TRUE; // Uses display name instead of the @handle
